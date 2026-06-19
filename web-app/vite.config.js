0d8fae8b7d0d8e9afead12d0730a90a2cf03bc8f
ERNI\pum <michal.puffler@bruker.com>
2026-06-19 16:42:00 +0200

    tiny change

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/captain-s-logbook/',
+  base: '/captain-s-logbook/',  // GitHub Pages subpath
   plugins: [react()],
 })
